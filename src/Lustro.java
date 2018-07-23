916c421aa9a51e33024d69f242bf5bbed13d51a4
Agnieszka Łozowska <aga.lozowska@wp.pl>
2018-07-23 17:00:56 +0200

    poprawiona pętla

--- src/Lustro.java
+++ src/Lustro.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Lustro {
@@ -6,10 +7,15 @@ public class Lustro {
         Random rand = new Random();
         for (int i = 0; i < tablica.length; i++) {
             tablica[i] = rand.nextInt(100);
-            System.out.println(tablica[i]);
         }
-        for (int i = tablica.length - 1; i >= 0; i--) {
-            System.out.println(tablica[i]);
-        }
+        System.out.println(Arrays.toString(tablica));
+        for (int i = 0; i < tablica.length * 2; i++) {
+            if (i < tablica.length) {
+                System.out.println(tablica[i]);
+            } else {
+                int x = tablica.length - (i - tablica.length) - 1;
+                System.out.println(tablica[x]);
+            }
         }
     }
+}
